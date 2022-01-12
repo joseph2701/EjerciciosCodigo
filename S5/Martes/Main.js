08770cb7247bfe2a4b40755bc769938bcb221b9a
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-01-11 19:16:59 -0500

    martesa

---
+++ S5/Martes/Main.js
@@ -0,0 +1,18 @@
+console.log("Hola Mundo");
+
+
+let nombreVariable=10;
+let edad=35;
+let nombre= "Joseph"
+let sexo="m";
+
+nombrevariable="cambiar el valor";
+console.log("nombrevariable",nombrevariable);
+
+nombrevariable="modificado denuevo";
+console.log("nombrevariable",nombrevariable);
+
+
+const PI=3.14
+const URL="https://www.google.com/";
+console.log("PI",PI);
